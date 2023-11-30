5772310bd3ef9f1c69fc96f0b3c42c142e281825
andwis17 <andwis17@gmail.com>
2023-11-30 18:56:44 +0100

    initial commit

---
+++ src/test/java/com/awplanner/PlannerCF/PlannerCfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.awplanner.PlannerCF;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlannerCfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
