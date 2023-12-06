d9a14dd155d3cba74179e435bfa510cd19fc93ad
andwis17 <andwis17@gmail.com>
2023-12-06 17:56:27 +0100

    createDayTest method implementation

--- src/test/java/com/plannercf/backend/service/DayServiceTests.java
+++ src/test/java/com/plannercf/backend/service/DayServiceTests.java
@@ -1,8 +1,14 @@
 package com.plannercf.backend.service;
 
+import com.plannercf.backend.domain.Day;
+import com.plannercf.backend.service.exception.RecordNotExistsException;
+import com.plannercf.backend.service.exception.TestNotCleaned;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import java.time.LocalDate;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
 @SpringBootTest
@@ -10,4 +16,25 @@ import org.springframework.boot.test.context.SpringBootTest;
 public class DayServiceTests {
     @Autowired
     private DayService service;
+
+    @Test
+    void createDayTest() throws RecordNotExistsException, TestNotCleaned {
+        //Given
+        service.createDay(LocalDate.of(2023,12,24));
+
+        //When
+        Day dayRetrieved = service.getDayByDate(LocalDate.parse("2023-12-24"));
+
+        //Then
+        assertEquals(LocalDate.of(2023,12,24), dayRetrieved.getDate());
+
+        //Clean
+        try {
+            service.deleteAll();
+        } catch (Exception err) {
+            throw new TestNotCleaned("createDayTest NOT cleaned");
+        }
+    }
+
+
 }
