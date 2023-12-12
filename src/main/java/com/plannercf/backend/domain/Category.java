a3a5f091fa1073ae5a7308400188dbfb18978802
andwis17 <andwis17@gmail.com>
2023-12-12 11:57:23 +0100

    Category.class implementatino

---
+++ src/main/java/com/plannercf/backend/domain/Category.java
@@ -0,0 +1,43 @@
+package com.plannercf.backend.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Objects;
+
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "CATEGORIES")
+public class Category extends BaseEntity{
+    @Column(name = "Name")
+    private String name;
+
+    public Category(Long id, String name) {
+        super(id);
+        this.name = name;
+    }
+
+    public Category(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+        Category category = (Category) o;
+        return Objects.equals(name, category.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), name);
+    }
+}
