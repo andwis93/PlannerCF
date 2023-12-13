7f153cd08d6f5b1e40330ebfadde8758a03ec199
andwis17 <andwis17@gmail.com>
2023-12-13 20:25:30 +0100

    CategoryControllerTests.class shouldUpdateCategory test method implementation

--- src/test/java/com/plannercf/backend/controller/CategoryControllerTests.java
+++ src/test/java/com/plannercf/backend/controller/CategoryControllerTests.java
@@ -16,7 +16,7 @@ import org.springframework.test.context.junit.jupiter.web.SpringJUnitWebConfig;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
-
+import java.util.List;
 import static org.mockito.Mockito.when;
 
 @SpringJUnitWebConfig
@@ -32,6 +32,9 @@ public class CategoryControllerTests {
 
     Category category = new Category(1L,"Test");
     CategoryDto categoryDto = new CategoryDto(1L,"TestDto");
+    List<Category> categories = List.of(category, new Category(2L, "Test2"));
+    List<CategoryDto> categoriesDto = List.of(categoryDto, new CategoryDto(2L, "TestDto2"));
+
 
     @Test
     void shouldSaveCategory() throws Exception {
@@ -47,7 +50,7 @@ public class CategoryControllerTests {
                         .post("/plannercf/category/Test")
                         .contentType(MediaType.APPLICATION_JSON)
                         .characterEncoding("UTF-8")
-                        .contentType(jsonContent))
+                        .content(jsonContent))
                 .andExpect(MockMvcResultMatchers.status().isOk())
                 .andExpect(MockMvcResultMatchers.jsonPath("$.id", Matchers.is(1)))
                 .andExpect(MockMvcResultMatchers.jsonPath("$.name", Matchers.is("TestDto")));
@@ -69,4 +72,45 @@ public class CategoryControllerTests {
                 .andExpect(MockMvcResultMatchers.jsonPath("$.id", Matchers.is(1)))
                 .andExpect(MockMvcResultMatchers.jsonPath("$.name", Matchers.is("TestDto")));
     }
+
+    @Test
+    void shouldGetAllCategories() throws Exception {
+        //Given
+        when(facade.getAllCategories()).thenReturn(categories);
+        when(mapper.mapToListCategoryDto(categories)).thenReturn(categoriesDto);
+
+        //When & Then
+        mockMvc
+                .perform(MockMvcRequestBuilders
+                        .get("/plannercf/category/all")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .characterEncoding("UTF-8"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$", Matchers.hasSize(2)))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[1].id", Matchers.is(2)))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[1].name", Matchers.is("TestDto2")));
+    }
+
+    @Test
+    void shouldUpdateCategory() throws Exception {
+        //Given
+        Category category1 = new Category(1L,"TestChanged");
+        CategoryDto categoryDto1 = new CategoryDto(1L,"TestDtoChanged");
+        when(facade.updateCategory(1L,categoryDto1)).thenReturn(category1);
+        when(mapper.mapToCategoryDto(category1)).thenReturn(categoryDto1);
+        Gson gson = new Gson();
+        String jsonContent = gson.toJson(categoryDto1);
+
+        //When & Then
+        mockMvc
+                .perform(MockMvcRequestBuilders
+                        .put("/plannercf/category/1")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .characterEncoding("UTF-8")
+                        .content(jsonContent))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.id", Matchers.is(1)))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.name", Matchers.is("TestDtoChanged")));
+    }
+
 }
